1f78cfe7194827e53321c3cd486b51e2365bb73e
ShafiArnob <shafiarnob@gmail.com>
2022-12-17 23:32:49 +0600

    card movement source to destination

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -10,7 +10,27 @@ function Kanban() {
   const [data, setData] = useState(mockData)
 
   const onDragEnd = result =>{
-    console.log(result);
+    if (!result.destination) return
+    const { source, destination } = result
+
+    if (source.droppableId !== destination.droppableId) {
+        const sourceColIndex = data.findIndex(e => e.id === source.droppableId)
+        const destinationColIndex = data.findIndex(e => e.id === destination.droppableId)
+
+        const sourceCol = data[sourceColIndex]
+        const destinationCol = data[destinationColIndex]
+
+        const sourceTask = [...sourceCol.tasks]
+        const destinationTask = [...destinationCol.tasks]
+
+        const [removed] = sourceTask.splice(source.index, 1)
+        destinationTask.splice(destination.index, 0, removed)
+
+        data[sourceColIndex].tasks = sourceTask
+        data[destinationColIndex].tasks = destinationTask
+
+        setData(data)
+    }
   }
 
   return (
